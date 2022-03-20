0cef437a8de6f60f03b413301ebb838d7381ceac
kuno <ayana.kuno@mixi.co.jp>
2022-03-20 20:55:59 +0900

    撮影と保存

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 class CameraPage extends StatelessWidget {
   const CameraPage({Key? key, required this.camera}) : super(key: key);
@@ -23,6 +26,7 @@ class Camera extends StatefulWidget {
 
 class _CameraState extends State<Camera> {
   late CameraController _cameraController;
+  XFile? imageFile;
 
   @override
   void initState() {
@@ -42,8 +46,54 @@ class _CameraState extends State<Camera> {
     super.dispose();
   }
 
+  takePicture2() async {
+    if (_cameraController.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      final file = await _cameraController.takePicture();
+      setState(() {
+        imageFile = file;
+      });
+      final Uint8List buffer = await file.readAsBytes();
+      await ImageGallerySaver.saveImage(buffer, name: file.name);
+      debugPrint('Picture saved to ${file.path}');
+    } on CameraException catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: CameraPreview(_cameraController));
+    if (!_cameraController.value.isInitialized) {
+      return const SizedBox.shrink();
+    }
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        Center(
+          child: CameraPreview(_cameraController),
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(imageFile.toString()),
+            IconButton(
+              onPressed: () async {
+                await takePicture2();
+              },
+              icon: const Icon(
+                Icons.camera,
+                size: 44.0,
+                semanticLabel: 'Take Picture',
+              ),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
