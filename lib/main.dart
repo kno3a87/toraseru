b661c2c1d144761715b24f1a46bf269bf69a6d1d
kuno <ayana.kuno@mixi.co.jp>
2022-03-27 14:10:35 +0900

    タブに分けた

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toraseru/camera_page.dart';
+import 'package:toraseru/web_rtc_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,13 +13,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CameraPage(),
+      home: const MainPage(),
     );
   }
 }
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const MyApp());
+}
+
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "カメラだよ",
+              ),
+              Tab(
+                text: "WebTRCだよ",
+              )
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            CameraPage(),
+            WebRTCPage(),
+          ],
+        ),
+      ),
+    );
+  }
 }
